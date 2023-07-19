f07c60c9cae52fbdd098bbd2f844e3e6d08f76ff
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-19 20:03:06 +0530

    added new shadcn component sheet

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 
-import { Button } from "@/components/ui/button";
+import MobileSidebar from "@/components/mobile-sidbar";
 
 export default function Navbar() {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
