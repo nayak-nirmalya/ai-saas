80135da762f8923a2dfe6616e1d8ab12900e54de
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-19 18:36:02 +0530

    added img assets to public folder

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Sidebar() {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      Sidebar
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-4">
+            <Image fill alt="LoGo" src="/logo.png" />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
