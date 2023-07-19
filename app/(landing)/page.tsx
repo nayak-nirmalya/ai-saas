2112ec8255ddfacb10bc4ca94ec3496fdc14c35c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-19 15:03:45 +0530

    added lgoin and register btn to landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function LandingPage() {
-  return <div>Landing Page (Unprotected!)</div>;
+  return (
+    <div>
+      Landing Page (Unprotected!)
+      <div>
+        <Link href="/sign-in">
+          <Button>Log In</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Sign Up</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
