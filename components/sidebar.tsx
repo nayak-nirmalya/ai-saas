ca3b9589b643e99a5bf8fb894279bd9631cc07fb
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-19 20:30:39 +0530

    added active class to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import React from "react";
 import { Montserrat } from "next/font/google";
@@ -63,6 +64,8 @@ const routes = [
 ];
 
 export default function Sidebar() {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -79,7 +82,12 @@ export default function Sidebar() {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
