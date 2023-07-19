f2291483f2af3e22ab90593a0ce64c0f51924772
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-19 20:49:20 +0530

    added onclick to dashboard tools list

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -12,6 +12,7 @@ import {
 
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const tools = [
   {
@@ -52,6 +53,8 @@ const tools = [
 ];
 
 export default function DashboardPage() {
+  const router = useRouter();
+
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -66,6 +69,7 @@ export default function DashboardPage() {
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
           <Card
+            onClick={() => router.push(tool.href)}
             key={tool.href}
             className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
           >
