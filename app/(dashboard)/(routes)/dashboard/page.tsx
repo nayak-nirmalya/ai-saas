c052bef84d47fe73cca1d8128cbc3385780a2c98
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-19 20:38:43 +0530

    added shadcn-ui@latest card

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import React from "react";
+
+import { MessageSquare } from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation"
+  }
+];
+
 export default function DashboardPage() {
   return (
     <div>
-      <p>Landing Page (Protected!)</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the Power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the Smartest AI - Explore the Power of AI
+        </p>
+      </div>
+
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <></>
+        ))}
+      </div>
     </div>
   );
 }
