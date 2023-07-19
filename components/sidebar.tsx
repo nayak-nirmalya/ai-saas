e4ddf27f7b716dd03d3f9539efa9c815a139995b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-19 19:28:24 +0530

    added dashboard to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,10 +4,20 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Montserrat } from "next/font/google";
+import { LayoutDashboard } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+];
 
 export default function Sidebar() {
   return (
@@ -21,6 +31,20 @@ export default function Sidebar() {
             Genius
           </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
