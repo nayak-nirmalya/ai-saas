1227db5c57b3099ac55cec0f0c98e69912de0f97
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-19 15:10:02 +0530

    added UserButton from clerk to dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
-  return <div>Landing Page (Protected!)</div>;
+  return (
+    <div>
+      <p>Landing Page (Protected!)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
