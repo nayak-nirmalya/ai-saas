5c55031bddd6b135620a497261c6752600750713
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-20 12:02:29 +0530

    added styling to heading comp

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { LucideIcon } from "lucide-react";
 
+import { cn } from "@/lib/utils";
+
 interface HeadingProps {
   title: string;
   description: string;
@@ -11,10 +13,20 @@ interface HeadingProps {
 
 export default function Heading({
   description,
-  icon,
+  icon: Icon,
   title,
   bgColor,
   iconColor
 }: HeadingProps) {
-  return <div>Heading</div>;
+  return (
+    <div className="px-4 lg:px-8 items-center gap-x-3 mb-8 flex">
+      <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+        <Icon className={cn("w-10 h-10", iconColor)} />
+      </div>
+      <div>
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </div>
+    </div>
+  );
 }
