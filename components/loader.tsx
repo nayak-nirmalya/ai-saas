09f6aa35afdc160ed032c9f18a214f28b89214a4
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-20 20:22:33 +0530

    show loader spin when isLoading

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Image from "next/image";
 
 export function Loader() {
-  return <div>Loader</div>;
+  return (
+    <div className="h-full flex flex-col gap-y-4 items-center justify-center">
+      <div className="w-10 h-10 relative animate-spin">
+        <Image alt="Loading" fill src="/logo.png" />
+      </div>
+      <p className="text-sm text-muted-foreground">Genius Is Thinking...</p>
+    </div>
+  );
 }
