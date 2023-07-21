a20900b9612f49ff016dfe40037b8b9db3f029b8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 20:37:18 +0530

    installed shadcn badge component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ export default async function DashboardLayout({
 
   return (
     <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
         <Sidebar apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72">
--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -2,6 +2,26 @@
 
 import React from "react";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
+
 export default function PorModal() {
-  return <div>PorModal</div>;
+  const proModal = useProModal();
+
+  return (
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+            Upgrade to Genius Pro
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
