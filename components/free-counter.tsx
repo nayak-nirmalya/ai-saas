77d6627da49ff3ee5100294bf8d60f96e032bd10
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 18:50:31 +0530

    added new shadcn comp progress

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -2,6 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 
+import { Card, CardContent } from "@/components/ui/card";
+import { MAX_FREE_COUNT } from "@/constants";
+
 export default function FreeCounter({
   apiLimitCount = 0
 }: {
@@ -15,5 +18,17 @@ export default function FreeCounter({
 
   if (!mounted) return null;
 
-  return <div>FreeCounter</div>;
+  return (
+    <div className="px-3">
+      <Card className="bg-white/10 border-0">
+        <CardContent className="py-6">
+          <div className="text-center text-sm text-white mb-4 space-y-2">
+            <p>
+              {apiLimitCount} / {MAX_FREE_COUNT} Free Generations
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
