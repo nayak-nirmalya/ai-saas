d41c228ff591570be83aaef7a0da75850115b126
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 18:29:49 +0530

    validate free tier on video api

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!
 });
@@ -16,6 +18,11 @@ export async function POST(req: Request) {
 
     if (!prompt) return new NextResponse("Prompt is Required", { status: 400 });
 
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial)
+      return new NextResponse("Free Trial Has Expired", { status: 403 });
+
     const response = await replicate.run(
       "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
       {
@@ -24,6 +31,8 @@ export async function POST(req: Request) {
         }
       }
     );
+
+    await increaseApiLimit();
 
     return NextResponse.json(response);
   } catch (error) {
