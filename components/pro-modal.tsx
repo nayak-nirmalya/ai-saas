035f7419f552193a0432255b86fadaa66b3bb445
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 20:51:34 +0530

    created pro modal with responsive design

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,16 +1,63 @@
 "use client";
 
 import React from "react";
+import {
+  MessageSquare,
+  Music,
+  ImageIcon,
+  VideoIcon,
+  Code,
+  Check,
+  Zap
+} from "lucide-react";
 
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle
 } from "@/components/ui/dialog";
 import { Badge } from "@/components/ui/badge";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 import { useProModal } from "@/hooks/use-pro-modal";
+import { cn } from "@/lib/utils";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10"
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10"
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10"
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10"
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10"
+  }
+];
 
 export default function PorModal() {
   const proModal = useProModal();
@@ -27,7 +74,29 @@ export default function PorModal() {
               </Badge>
             </div>
           </DialogTitle>
+          <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+            {tools.map((tool) => (
+              <Card
+                key={tool.label}
+                className="p-3 border-black/5 flex items-center justify-between"
+              >
+                <div className="flex items-center gap-x-4">
+                  <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                    <tool.icon className={cn("w-6 h-6", tool.color)} />
+                  </div>
+                  <div className="font-semibold text-sm">{tool.label}</div>
+                </div>
+                <Check className="text-primary w-5 h-5" />
+              </Card>
+            ))}
+          </DialogDescription>
         </DialogHeader>
+        <DialogFooter>
+          <Button size="lg" variant="premium" className="w-full">
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
