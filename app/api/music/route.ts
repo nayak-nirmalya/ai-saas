b387c82be7bbfbbc834c8ec11f22f1a8834eeefa
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 18:29:15 +0530

    validate free tier on music api

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!
 });
@@ -16,6 +18,11 @@ export async function POST(req: Request) {
 
     if (!prompt) return new NextResponse("Prompt is Required", { status: 400 });
 
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial)
+      return new NextResponse("Free Trial Has Expired", { status: 403 });
+
     const response = await replicate.run(
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
       {
@@ -24,6 +31,8 @@ export async function POST(req: Request) {
         }
       }
     );
+
+    await increaseApiLimit();
 
     return NextResponse.json(response);
   } catch (error) {
