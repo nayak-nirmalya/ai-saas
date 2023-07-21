37c1ba23f409fc2cc3fc2f85a1dd5ddc324ce1b9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 18:13:24 +0530

    added util to increaseApiLimit

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -1,1 +1,28 @@
-import second from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+import { MAX_FREE_COUNT } from "@/constants";
+
+export const increaseApiLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) return;
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: { userId }
+  });
+
+  if (userApiLimit) {
+    await prismadb.userApiLimit.update({
+      where: { userId },
+      data: { count: userApiLimit.count + 1 }
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: {
+        userId,
+        count: 1
+      }
+    });
+  }
+};
