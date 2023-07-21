a1d40e23999edfef5d6ce70e0ac3a0678dfd88f8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 12:27:38 +0530

    added replicate api to gen video

---
+++ app/api/video/route.ts
@@ -0,0 +1,33 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN!
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    if (!userId) return new NextResponse("UnAuthorized", { status: 401 });
+
+    if (!prompt) return new NextResponse("Prompt is Required", { status: 400 });
+
+    const response = await replicate.run(
+      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+      {
+        input: {
+          prompt
+        }
+      }
+    );
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error("[VIDEO_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
