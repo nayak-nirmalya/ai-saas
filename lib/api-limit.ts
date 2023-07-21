7a2f71b783491e387c0492245a2f1b673aa157b3
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 18:35:22 +0530

    added new util fn getApiLimitCount

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -42,3 +42,17 @@ export const checkApiLimit = async () => {
     return false;
   }
 };
+
+export const getApiLimitCount = async () => {
+  const { userId } = auth();
+
+  if (!userId) return 0;
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: { userId }
+  });
+
+  if (!userApiLimit) return 0;
+
+  return userApiLimit.count;
+};
