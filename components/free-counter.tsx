04a72f0db31ac78d273dafec3ab3a4fb82fc2f07
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-21 18:54:27 +0530

    show progress bar and upgrade btn on sidebar

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { Zap } from "lucide-react";
 
 import { Card, CardContent } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
+
 import { MAX_FREE_COUNT } from "@/constants";
 
 export default function FreeCounter({
@@ -26,7 +30,14 @@ export default function FreeCounter({
             <p>
               {apiLimitCount} / {MAX_FREE_COUNT} Free Generations
             </p>
+            <Progress
+              className="h-3"
+              value={(apiLimitCount / MAX_FREE_COUNT) * 100}
+            />
           </div>
+          <Button className="w-full">
+            Upgrade <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
         </CardContent>
       </Card>
     </div>
