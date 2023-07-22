150c622ab087924f3808e4564ffa6d8b60d0c0d9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-22 08:50:15 +0530

    added api route for invoice.payment_succeeded

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -42,4 +42,22 @@ export async function POST(req: Request) {
       }
     });
   }
+
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    await prismadb.userSubscription.update({
+      where: {
+        stripeSubscriptionId: subscription.id
+      },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000)
+      }
+    });
+  }
+
+  return new NextResponse(null, { status: 200 });
 }
