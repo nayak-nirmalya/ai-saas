e6a2030d27d7a4957b0f608591c0ed97a5129807
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-22 11:27:15 +0530

    bug fix: upgrade modal for pro user in video

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
 import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!
@@ -19,8 +20,9 @@ export async function POST(req: Request) {
     if (!prompt) return new NextResponse("Prompt is Required", { status: 400 });
 
     const freeTrial = await checkApiLimit();
+    const isPro = await checkSubscription();
 
-    if (!freeTrial)
+    if (!freeTrial && !isPro)
       return new NextResponse("Free Trial Has Expired", { status: 403 });
 
     const response = await replicate.run(
@@ -32,7 +34,7 @@ export async function POST(req: Request) {
       }
     );
 
-    await increaseApiLimit();
+    if (!isPro) await increaseApiLimit();
 
     return NextResponse.json(response);
   } catch (error) {
