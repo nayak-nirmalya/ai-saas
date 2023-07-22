4b99965c7b3aa1703df0c2927f3eb1c33f09aa66
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-22 11:45:54 +0530

    show toast error on video page

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -7,6 +7,7 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
 
 import Heading from "@/components/heading";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
@@ -43,6 +44,7 @@ export default function VideoPage() {
       form.reset();
     } catch (error: any) {
       if (error?.response?.status === 403) proModal.onOpen();
+      else toast.error("Something went wrong.");
     } finally {
       router.refresh();
     }
